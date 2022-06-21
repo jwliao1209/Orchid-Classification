e25f2b6fe1a18538de86c92780d5d8abed67c709
Jia-Wei-Liao <55970911+Jia-Wei-Liao@users.noreply.github.com>
2022-06-21 19:57:04 +0800

    Delete inference_2.sh

--- inference_2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup python3 inference_tta_split.py --mode private --checkpoint 06-06-10-04-43 06-06-10-03-00 06-06-09-29-55 06-06-07-46-11 06-06-05-29-11 --topk 1 1 1 1 1 > infer.txt &
